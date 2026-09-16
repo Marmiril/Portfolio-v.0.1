ff599710ff8790604124c78c5203d063b57fa568
Marmiril <angel.plata@outlook.es>
2026-09-16 21:03:11 +0200

    Improve responsive layout for mobile and tablet

--- app/views/layouts/base.php
+++ app/views/layouts/base.php
@@ -11,6 +11,7 @@ if(!isset($view)) {
 
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Portfolio</title>
         
         <link href="https://fonts.googleapis.com/css2?family=Cinzel&display=swap" rel="stylesheet">
