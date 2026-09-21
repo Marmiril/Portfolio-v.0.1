f5612da3fb6fca1b9c471a4aa8b794e140884a4d
Marmiril <angel.plata@outlook.es>
2026-09-21 18:40:50 +0200

    Some fixes

--- public/apps/TaleTeller/views/layouts/base.php
+++ public/apps/TaleTeller/views/layouts/base.php
@@ -4,6 +4,7 @@
 <html lang="es">
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
       	<meta name="darkreader-lock">
         <title>TaleTeller</title>
 
